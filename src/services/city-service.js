a5618c221d7e95b715aed537887eb8e2763ab192
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2023-08-31 18:25:16 +0530

    added logic in city-service

---
+++ src/services/city-service.js
@@ -0,0 +1,55 @@
+const { CityRepository } = require('../repository/index')
+
+class CityService {
+  // here we are using constructor , in repository we are not using constructor
+  constructor() {
+    this.cityRepository = new CityRepository()
+  }
+  async createCity(data) {
+    try {
+      const city = await this.cityRepository.createCity(data)
+      return city
+    } catch (error) {
+      console.log('something went wrong at service layer')
+      throw { error }
+    }
+  }
+  async deleteCity(cityId) {
+    try {
+      const response = await this.cityRepository.deleteCity(cityId)
+      return response
+    } catch (error) {
+      console.log('something went wrong at service layer')
+      throw { error }
+    }
+  }
+  async updateCity(data, cityId) {
+    try {
+      const city = await this.cityRepository.updateCity(data, cityId)
+      return city
+    } catch (error) {
+      console.log('something went wrong at service layer')
+      throw { error }
+    }
+  }
+  async getCity(cityId) {
+    try {
+      const city = await this.cityRepository.getCity(cityId)
+      return city
+    } catch (error) {
+      console.log('something went wrong at service layer')
+      throw { error }
+    }
+  }
+  async getAllCities() {
+    try {
+      const cities = await this.cityRepository.getAllCities()
+      return cities
+    } catch (error) {
+      console.log('something went wrong at service layer')
+      throw { error }
+    }
+  }
+}
+
+module.exports = CityService
