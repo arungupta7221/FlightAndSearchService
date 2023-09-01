24e375b90a1deba34a3a6843bdcccfa633692fe0
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2023-09-01 18:15:30 +0530

    added router and now create city api is working

--- src/index.js
+++ src/index.js
@@ -3,22 +3,15 @@ const { PORT } = require('./config/serverConfig')
 var bodyParser = require('body-parser')
 const CityRepository = require('./repository/city-repository')
 
+const ApiRoutes = require('./routes/index')
+
 const setupAndStartServer = async () => {
   const app = express()
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
+  app.use('/api', ApiRoutes)
   app.listen(PORT, () => {
     console.log(`server is running on PORT ${PORT}`)
   })
-  const city = new CityRepository()
-  // city.createCity({
-  //   name: 'Mumbai',
-  // })
-  // await city.createCity({
-  //   name: 'Banglore',
-  // })
-  // city.deleteCity({
-  //   cityId: 2,
-  // })
 }
 setupAndStartServer()
