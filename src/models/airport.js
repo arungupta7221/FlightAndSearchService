0e3efd9986c8eec1c388fc19a27ca874dd0cee03
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2023-09-05 10:04:48 +0530

    added airport model and created one to many association between airport and city table

--- src/models/airport.js
+++ src/models/airport.js
@@ -1,30 +1,36 @@
 'use strict'
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.Airport, {
+      this.belongsTo(models.City, {
         foreignKey: 'cityId',
+        onDelete: 'CASCADE', // this property will allow that if we delete a city so all the airports belonging to that city should be delete
       })
     }
   }
-  City.init(
+  Airport.init(
     {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
       },
+      address: DataTypes.STRING,
+      cityId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
     {
       sequelize,
-      modelName: 'City',
+      modelName: 'Airport',
     },
   )
-  return City
+  return Airport
 }
